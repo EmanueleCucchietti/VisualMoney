1a0dcf4258f770d00577e2cb4da22a095c7dc399
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2023-11-21 23:15:36 +0100

    Changed Error interceptor bcs was creating problems on httpclient requests errors

--- Src/WebClientApp/WebClient/src/app/_helpers/error.interceptor.ts
+++ Src/WebClientApp/WebClient/src/app/_helpers/error.interceptor.ts
@@ -35,19 +35,17 @@ export class ErrorInterceptor implements HttpInterceptor {
         request: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
-        // this.authenticationService = this.injector.get(AuthenticationService);
 
         return next.handle(request).pipe(
             catchError((err) => {
                 if ([401, 403].indexOf(err.status) !== -1) {
-                    // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-                    // this.authenticationService.logout();
 
                     return this.HandleAuthenticationError(request, next);
                 }
 
-                const error = err.error.message || err.statusText;
-                return throwError(() => error);
+				// const error = err.error.message || err.statusText;
+
+                return throwError(() => err);
             })
         );
     }
