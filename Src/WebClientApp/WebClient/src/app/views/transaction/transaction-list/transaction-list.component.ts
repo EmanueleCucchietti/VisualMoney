27e08e31081ae899dbf08989d1a4659320d789bc
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2023-12-14 23:08:44 +0100

    Corrected padding for lists, added checks on empty lists

--- Src/WebClientApp/WebClient/src/app/views/transaction/transaction-list/transaction-list.component.ts
+++ Src/WebClientApp/WebClient/src/app/views/transaction/transaction-list/transaction-list.component.ts
@@ -3,9 +3,19 @@ import { TransactionModel } from 'src/app/_models/Transaction/transactionModel';
 
 @Component({
   selector: 'app-transaction-list',
+  host: { 'class': 'childRouteFlex' },
   templateUrl: './transaction-list.component.html',
   styleUrls: ['./transaction-list.component.css']
 })
 export class TransactionListComponent {
 	@Input() transactions: TransactionModel[] = [];
+
+	loading : boolean = true;
+
+	ngOnInit() {
+		if (this.transactions.length == 0) {
+			this.loading = false;
+		}
+	}
+
 }
--- Src/WebClientApp/WebClient/src/app/views/wallet/wallet-list/wallet-list.component.ts
+++ Src/WebClientApp/WebClient/src/app/views/wallet/wallet-list/wallet-list.component.ts
@@ -2,24 +2,22 @@ import { Component } from '@angular/core';
 import { WalletService } from 'src/app/_services';
 
 @Component({
-  selector: 'app-wallet-list',
-  host: { 'class': 'childRouteFlex' },
-  templateUrl: './wallet-list.component.html',
-  styleUrls: ['./wallet-list.component.css']
+    selector: 'app-wallet-list',
+    host: { class: 'childRouteFlex' },
+    templateUrl: './wallet-list.component.html',
+    styleUrls: ['./wallet-list.component.css']
 })
 export class WalletListComponent {
+    constructor(public walletService: WalletService) {
+        if (walletService.wallets.length == 0) {
+            walletService.getWalletsFromServer().subscribe(() => {
+                walletService.wallets.push(...walletService.wallets);
+                walletService.wallets.push(...walletService.wallets);
+                walletService.wallets.push(...walletService.wallets);
+				this.loading = false;
+            });
+        }
+    }
 
-  constructor(public walletService : WalletService) {
-	if(walletService.wallets.length == 0){
-		walletService.getWalletsFromServer().subscribe(()=>{
-			walletService.wallets.push(...walletService.wallets);
-			walletService.wallets.push(...walletService.wallets);
-			walletService.wallets.push(...walletService.wallets);
-		});
-	}
-
-
-  }
-
-
+	loading : boolean = true;
 }
