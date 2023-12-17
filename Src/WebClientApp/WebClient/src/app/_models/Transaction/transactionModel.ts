e4a6ada78f9d780e692e97b8f6d2f06c82e76465
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2023-12-17 22:42:19 +0100

    Added Date to transactionModel due to merge with server update

--- Src/WebClientApp/WebClient/src/app/_models/Transaction/transactionModel.ts
+++ Src/WebClientApp/WebClient/src/app/_models/Transaction/transactionModel.ts
@@ -3,13 +3,15 @@ export class TransactionModel {
 	name: string;
 	amount: number;
 	currencyCode: string;
+	date: Date;
 	isIncome: boolean;
 	idWallet: number;
 
-	constructor(name: string = '', amount: number = 0, currencyCode: string = '', isIncome: boolean = true, idWallet: number = 0) {
+	constructor(name: string = '', amount: number = 0, currencyCode: string = '', date : Date = new Date(),isIncome: boolean = true, idWallet: number = 0) {
 		this.name = name;
 		this.amount = amount;
 		this.currencyCode = currencyCode;
+		this.date = date;
 		this.isIncome = isIncome;
 		this.idWallet = idWallet;
 	}
