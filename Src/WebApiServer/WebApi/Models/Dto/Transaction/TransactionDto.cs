c290f91f661e6e1006a4b913b1c4839d9afd59a8
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2023-12-17 22:29:53 +0100

    Added Date Return to Transactions

--- Src/WebApiServer/WebApi/Models/Dto/Transaction/TransactionDto.cs
+++ Src/WebApiServer/WebApi/Models/Dto/Transaction/TransactionDto.cs
@@ -7,6 +7,7 @@ namespace WebApi.Models.Dto.Transaction
         public string Name { get; set; }
         public decimal Amount { get; set; }
         public string CurrencyCode { get; set; }
+        public DateTime Date { get; set; }
         public bool IsIncome { get; set; }
         public int IdWallet { get; set; }
     }
